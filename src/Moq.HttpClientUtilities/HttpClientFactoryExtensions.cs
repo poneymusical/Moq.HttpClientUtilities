75bffd3a469a45a9ef45bb097e32819b927cb519
Guillaume <guillaume.dujardin@outlook.com>
2021-08-26 00:13:59 +0200

    Added comments

--- src/Moq.HttpClientUtilities/HttpClientFactoryExtensions.cs
+++ src/Moq.HttpClientUtilities/HttpClientFactoryExtensions.cs
@@ -5,16 +5,28 @@ namespace Moq.HttpClientUtilities
 {
     public static class HttpClientFactoryExtensions
     {
-        public static HttpClient CreateHttpClient(this Mock<HttpMessageHandler> handler, string baseUri) =>
+        private const string DefaultUri = "http://unittest";
+        
+        /// <summary>
+        /// Creates a HttpClient that uses the mocked HttpMessageHandler.
+        /// </summary>
+        /// <param name="baseUri">The base URI passed to the HttpClient. This can be any valid URI; it will not be used.</param>
+        /// <returns>A new instance of HttpClient that uses the mocked HttpMessageHandler.</returns>
+        public static HttpClient CreateHttpClient(this Mock<HttpMessageHandler> handler, string baseUri = DefaultUri) =>
             CreateHttpClient(handler, new Uri(baseUri));
 
-        public static HttpClient CreateHttpClient(this Mock<HttpMessageHandler> handler, Uri baseUri) => 
+        private static HttpClient CreateHttpClient(this Mock<HttpMessageHandler> handler, Uri baseUri) => 
             new HttpClient(handler.Object) { BaseAddress = baseUri };
 
-        public static IHttpClientFactory CreateHttpClientFactory(this Mock<HttpMessageHandler> handler, string baseUri) =>
+        /// <summary>
+        /// Creates a IHttpClientFactory that always returns a HttpClient that uses the mocked HttpMessageHandler.
+        /// </summary>
+        /// <param name="baseUri">The base URI passed to the HttpClient. This can be any valid URI; it will not be used.</param>
+        /// <returns>A new instance of HttpClient that uses the mocked HttpMessageHandler.</returns>
+        public static IHttpClientFactory CreateHttpClientFactory(this Mock<HttpMessageHandler> handler, string baseUri = DefaultUri) =>
             CreateHttpClientFactory(handler, new Uri(baseUri));
 
-        public static IHttpClientFactory CreateHttpClientFactory(this Mock<HttpMessageHandler> handler, Uri baseUri)
+        private static IHttpClientFactory CreateHttpClientFactory(this Mock<HttpMessageHandler> handler, Uri baseUri)
         {
             var httpClient = handler.CreateHttpClient(baseUri);
             
--- src/Moq.HttpClientUtilities/VerifyExtensions.cs
+++ src/Moq.HttpClientUtilities/VerifyExtensions.cs
@@ -7,6 +7,13 @@ namespace Moq.HttpClientUtilities
 {
     public static class VerifyExtensions
     {
+        /// <summary>
+        /// Verifies that a HTTP request was sent to the specified uri fragment with the specified verb.
+        /// Throws an exception if the conditions were not met. 
+        /// </summary>
+        /// <param name="method">The HTTP method (GET, POST, ...)</param>
+        /// <param name="uri">The uri fragment (excluding the base path)</param>
+        /// <param name="times">How many times the request must have been sent</param>
         public static void Verify(this Mock<HttpMessageHandler> handler,
             HttpMethod method, string uri, Times times)
         {
